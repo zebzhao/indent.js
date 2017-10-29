311fbc2f3842ec9d27e0c1dfdbd58189e3037333
zeb <zeb.zhao@gamecheetah.net>
2017-10-28 23:26:10 -0400

    upgrade packages and method name

--- tools/run-file.js
+++ tools/run-file.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
 var indent = require('./../lib/indent.js');
-var self = this;
 
 fs.readFile( __dirname + '/file.js', function (err, data) {
     if (err)
         throw err;
     console.log(
-        indent.indentJS(data.toString(), '  ')
+        indent.js(data.toString(), '  ')
     );
 });
