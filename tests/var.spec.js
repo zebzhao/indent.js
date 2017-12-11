1e1cfec723a3ea838316c0a80032868f0fb40727
zeb <zeb.zhao@gamecheetah.net>
2017-12-11 01:12:19 -0500

    add let and const to var rule

---
+++ tests/var.spec.js
@@ -0,0 +1,77 @@
+describe('statements.spec.js', function() {
+  it('should handle indent var declarations', function () {
+    var input = hereDoc(function () {/*!
+var
+// See here for display values: https://developer.mozilla.org/en-US/docs/CSS/display
+rdisplayswap = /^(none|table(?!-c[ea]).+)/,
+cssShow = { position: "absolute", visibility: "hidden", display: "block" },
+cssNormalTransform = {
+letterSpacing: "0",
+fontWeight: "400"
+},
+cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
+emptyStyle = document.createElement( "div" ).style;
+var
+rkeyEvent = /^key/,
+rmouseEvent = /^(?:mouse|pointer|contextmenu|drag|drop)|click/,
+rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
+*/
+    });
+    var expected = hereDoc(function () {/*!
+var
+  // See here for display values: https://developer.mozilla.org/en-US/docs/CSS/display
+  rdisplayswap = /^(none|table(?!-c[ea]).+)/,
+  cssShow = { position: "absolute", visibility: "hidden", display: "block" },
+  cssNormalTransform = {
+    letterSpacing: "0",
+    fontWeight: "400"
+  },
+  cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
+  emptyStyle = document.createElement( "div" ).style;
+var
+  rkeyEvent = /^key/,
+  rmouseEvent = /^(?:mouse|pointer|contextmenu|drag|drop)|click/,
+  rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
+*/
+    });
+    expect(indent.indentJS(input, '  ')).toEqual(expected);
+  });
+
+  it('should handle indent let declarations', function () {
+    var input = hereDoc(function () {/*!
+let
+// See here for display values: https://developer.mozilla.org/en-US/docs/CSS/display
+rdisplayswap = /^(none|table(?!-c[ea]).+)/,
+cssShow = { position: "absolute", visibility: "hidden", display: "block" },
+cssNormalTransform = {
+letterSpacing: "0",
+fontWeight: "400"
+},
+cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
+emptyStyle = document.createElement( "div" ).style;
+let
+rkeyEvent = /^key/,
+rmouseEvent = /^(?:mouse|pointer|contextmenu|drag|drop)|click/,
+rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
+*/
+    });
+    var expected = hereDoc(function () {/*!
+let
+  // See here for display values: https://developer.mozilla.org/en-US/docs/CSS/display
+  rdisplayswap = /^(none|table(?!-c[ea]).+)/,
+  cssShow = { position: "absolute", visibility: "hidden", display: "block" },
+  cssNormalTransform = {
+    letterSpacing: "0",
+    fontWeight: "400"
+  },
+  cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
+  emptyStyle = document.createElement( "div" ).style;
+let
+  rkeyEvent = /^key/,
+  rmouseEvent = /^(?:mouse|pointer|contextmenu|drag|drop)|click/,
+  rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
+*/
+    });
+    expect(indent.indentJS(input, '  ')).toEqual(expected);
+  });
+});
