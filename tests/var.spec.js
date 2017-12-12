e76c45396a29facaf7e1c71b9ee8c1a89b7e6668
zeb <zeb.zhao@gamecheetah.net>
2017-12-11 22:09:49 -0500

    change tests

--- tests/var.spec.js
+++ tests/var.spec.js
@@ -40,36 +40,30 @@ var
   it('should handle indent let declarations', function () {
     var input = hereDoc(function () {/*!
 let
-// See here for display values: https://developer.mozilla.org/en-US/docs/CSS/display
-rdisplayswap = /^(none|table(?!-c[ea]).+)/,
-cssShow = { position: "absolute", visibility: "hidden", display: "block" },
-cssNormalTransform = {
-letterSpacing: "0",
-fontWeight: "400"
-},
-cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
-emptyStyle = document.createElement( "div" ).style;
-let
-rkeyEvent = /^key/,
-rmouseEvent = /^(?:mouse|pointer|contextmenu|drag|drop)|click/,
-rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
+test1,
+test2,
+test3;
+
+let test1,
+test2,
+test3 = 10,
+test4 = [10,
+12,
+13];
 */
     });
     var expected = hereDoc(function () {/*!
 let
-  // See here for display values: https://developer.mozilla.org/en-US/docs/CSS/display
-  rdisplayswap = /^(none|table(?!-c[ea]).+)/,
-  cssShow = { position: "absolute", visibility: "hidden", display: "block" },
-  cssNormalTransform = {
-    letterSpacing: "0",
-    fontWeight: "400"
-  },
-  cssPrefixes = [ "Webkit", "O", "Moz", "ms" ],
-  emptyStyle = document.createElement( "div" ).style;
-let
-  rkeyEvent = /^key/,
-  rmouseEvent = /^(?:mouse|pointer|contextmenu|drag|drop)|click/,
-  rtypenamespace = /^([^.]*)(?:\.(.+)|)/;
+  test1,
+  test2,
+  test3;
+
+let test1,
+  test2,
+  test3 = 10,
+  test4 = [10,
+    12,
+    13];
 */
     });
     expect(indent.indentJS(input, '  ')).toEqual(expected);
