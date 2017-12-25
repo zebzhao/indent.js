813f5a7281c9008dcd1e415dff8424e65e001d85
zeb <zeb.zhao@gamecheetah.net>
2017-12-25 00:22:44 -0500

    remove countdown

---
+++ tests/javascript/statements.spec.js
@@ -0,0 +1,43 @@
+var sut = require('../../lib/indent').js;
+var expect = require('chai').expect;
+var ts = '  ';
+
+describe('statements', function () {
+
+  it('if: single line', function () {
+    var expected = `
+if (a)
+  statement;
+else if (b)
+  statement;
+else
+  for (var i=0; i<10; i++;)
+    statement
+`;
+    expect(sut(expected, ts)).to.equal(
+      expected.replace(/\r*\n/g, '\r\n'));
+  });
+
+  it('if: single line with object', function () {
+    var expected = `
+if (a)
+  b = {
+    c: 0,
+    d: 1
+  }
+`;
+    expect(sut(expected, ts)).to.equal(
+      expected.replace(/\r*\n/g, '\r\n'));
+  });
+
+  it('if: multiple lines before expression', function () {
+    var expected = `
+if (a)
+  
+  
+  statement;
+`;
+    expect(sut(expected, ts)).to.equal(
+      expected.replace(/\r*\n/g, '\r\n'));
+  });
+});
